636a1f1c9e44b72cc28d6b0a3f4f6453ad3293cb
zihang <1041361162@qq.com>
2022-02-09 22:21:32 +0800

    blog app commit

---
+++ blog-admin/src/main/java/com/gzh/blog/admin/mapper/PermissionMapper.java
@@ -0,0 +1,9 @@
+package com.gzh.blog.admin.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.gzh.blog.admin.pojo.Permission;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PermissionMapper extends BaseMapper<Permission> {
+}
